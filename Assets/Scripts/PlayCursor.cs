b65be27a6c1e42654419d3b6db8c09e95b71dbaf
wayven1234 <hanjun9035@naver.com>
2025-05-29 23:18:01 +0900

    20250529 조한준
    마우스 커서 플레이 도중에 사라지게하기

---
+++ Assets/Scripts/PlayCursor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCursor : MonoBehaviour
+{
+    public Canvas canvas;
+    public RectTransform cursorImage;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        cursorImage.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        cursorImage.position = mousePos;
+    }
+}
